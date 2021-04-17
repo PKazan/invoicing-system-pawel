e0a50a75782359e83525ca90a005954712f3dcbe
Paweł Kazanecki <kazan87@gmail.com>
2021-04-18 00:08:11 +0200

    created new file Company

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,14 +1,18 @@
 package pl.futurecollars.invoicing.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class Company {
 
     private String identificationNumber;
     private String address;
     private String name;
+
+    public Company(String identificationNumber, String address, String name) {
+        this.identificationNumber = identificationNumber;
+        this.address = address;
+        this.name = name;
+    }
 
 }
