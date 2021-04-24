19d4479791d493c0c69c02d6b4485515dbed006d
Paweł Kazanecki <kazan87@gmail.com>
2021-04-24 02:38:58 +0200

    Created FileService, JsonService, IdService and new implementation of Database with tests

--- app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Invoice.java
@@ -3,20 +3,22 @@ package pl.futurecollars.invoicing.model;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Invoice {
 
     private int id;
     private LocalDate date;
     private Company buyer;
     private Company seller;
-    private List<InvoiceEntry> invoiceEntries;
+    private List<InvoiceEntry> entries;
 
     public Invoice(LocalDate date, Company buyer, Company seller, List<InvoiceEntry> entries) {
         this.date = date;
         this.buyer = buyer;
         this.seller = seller;
-        this.invoiceEntries = entries;
+        this.entries = entries;
     }
 }
