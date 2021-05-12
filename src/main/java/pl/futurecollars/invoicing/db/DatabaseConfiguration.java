9c30fbf1069e14feeccd90fd0423e3210ab8c27c
Paweł Kazanecki <kazan87@gmail.com>
2021-05-12 02:27:30 +0200

    Added Spring Profiles

--- src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/db/DatabaseConfiguration.java
@@ -1,24 +1,51 @@
 package pl.futurecollars.invoicing.db;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.futurecollars.invoicing.db.file.FileBasedDatabase;
 import pl.futurecollars.invoicing.db.file.IdService;
+import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.util.FilesService;
 import pl.futurecollars.invoicing.util.JsonService;
+
+@Slf4j
 @Configuration
 public class DatabaseConfiguration {
 
     @Bean
-    FileBasedDatabase fileBasedDatabase(IdService idService, FilesService filesService, JsonService jsonService) throws IOException {
-        Path filePath = Files.createTempFile("prefix", ".txt");
+    @ConditionalOnProperty(name = "invoices.database", havingValue = "file")
+    public Database fileBasedDatabase(
+        IdService idService,
+        FilesService filesService,
+        JsonService jsonService,
+        @Value("${invoices.database.directory}") String databaseDirectory,
+        @Value("{invoices.database.invoices.file}") String invoicesFile
+    ) throws IOException {
+        log.debug("File db selected");
+        Path filePath = Files.createTempFile(databaseDirectory, invoicesFile);
         return new FileBasedDatabase(filePath, idService, filesService, jsonService);
     }
+
     @Bean
-    IdService idService(FilesService filesService) throws IOException {
-        Path filePath = Files.createTempFile("idPrefix", ".txt");
+    @ConditionalOnProperty(name = "invoices.database", havingValue = "memory")
+    public Database inMemoryDatabase() {
+        log.debug("In mem db selected");
+        return new InMemoryDatabase();
+    }
+
+    @Bean
+    public IdService idService(
+        FilesService filesService,
+        @Value("${invoices.database.directory}") String databaseDirectory,
+        @Value("${invoices.database.id.file}") String idFile
+    ) throws IOException {
+        Path filePath = Files.createTempFile(databaseDirectory, idFile);
         return new IdService(filePath, filesService);
     }
 }
