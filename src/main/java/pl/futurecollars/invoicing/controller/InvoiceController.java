42df00746770c18ccbaa0ef25459c2620d47d84d
PKazan <77175128+PKazan@users.noreply.github.com>
2021-05-15 20:26:03 +0200

    Added Springfox, created documentation for application (#16)
    * Added Springfox, created documentation for application
    
    * Corrected errors found by checkstyle

--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,22 +1,17 @@
 package pl.futurecollars.invoicing.controller;
 
+import io.swagger.annotations.Api;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
-@RequestMapping("/invoices")
-
-public class InvoiceController {
+@Api(tags = {"Invoice-Controller"})
+public class InvoiceController implements InvoiceApi {
 
     InvoiceService invoiceService;
 
@@ -24,24 +19,24 @@ public class InvoiceController {
         this.invoiceService = invoiceService;
     }
 
-    @PostMapping
+    @Override
     public int addInvoice(@RequestBody Invoice invoice) {
         return invoiceService.save(invoice);
     }
 
-    @GetMapping(produces = {"application/json;charset=UTF-8"})
+    @Override
     public List<Invoice> getAll() {
         return invoiceService.getAll();
     }
 
-    @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+    @Override
     public ResponseEntity<Invoice> getById(@PathVariable int id) {
         return invoiceService.getById(id)
             .map(invoice -> ResponseEntity.ok().body(invoice))
             .orElse(ResponseEntity.notFound().build());
     }
 
-    @PutMapping("/{id}")
+    @Override
     public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
         try {
             return invoiceService.update(id, invoice)
@@ -52,7 +47,7 @@ public class InvoiceController {
         }
     }
 
-    @DeleteMapping("/{id}")
+    @Override
     public ResponseEntity<?> delete(@PathVariable int id) {
         return invoiceService.delete(id)
         .map(name -> ResponseEntity.noContent().build())
--- src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.futurecollars.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,11 @@ import lombok.NoArgsConstructor;
 
 public class Company {
 
+    @ApiModelProperty(value = "Tax identification number", required = true, example = "555-555-55-55")
     private String taxIdentificationNumber;
+    @ApiModelProperty(value = "Company address", required = true, example = "Mazowiecka 134, 32-525, Radzionków")
     private String address;
+    @ApiModelProperty(value = "Company name", required = true, example = "Invoice House Ltd.")
     private String name;
 
 }
