e091701f02d36b9eec71885466fa1567c8cca6a8
Paweł Kazanecki <kazan87@gmail.com>
2021-05-19 15:12:45 +0200

    Increased tests coverage, added tests

--- src/test/groovy/pl/futurecollars/invoicing/controller/taxes/TaxCalculatorControllerIntegrationTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/controller/taxes/TaxCalculatorControllerIntegrationTest.groovy
@@ -32,28 +32,83 @@ class TaxCalculatorControllerIntegrationTest extends Specification {
         getAllInvoices().each {invoice -> deleteInvoiceById(invoice.id) }
     }
 
-    def "returned correct values when tax identification number was found"() {
+    def "returned correct values for seller"() {
         given:
         addUniqueInvoices(5)
 
         when:
-        def taxCalculatorResponse = calculateTax("5")
+        def taxCalculatorResponse = calculateTax("4")
 
         then:
-        taxCalculatorResponse.income == 15000
+        taxCalculatorResponse.income == 10000
         taxCalculatorResponse.costs == 0
-        taxCalculatorResponse.earnings == 15000
-        taxCalculatorResponse.incomingVat == 1200
+        taxCalculatorResponse.earnings == 10000
+        taxCalculatorResponse.incomingVat == 800
         taxCalculatorResponse.outgoingVat == 0
-        taxCalculatorResponse.vatToPay == 1200
+        taxCalculatorResponse.vatToPay == 800
 
+        when:
+        addUniqueInvoices(5)
+        taxCalculatorResponse = calculateTax("4")
 
+        then:
+        taxCalculatorResponse.income == 20000
+        taxCalculatorResponse.costs == 0
+        taxCalculatorResponse.earnings == 20000
+        taxCalculatorResponse.incomingVat == 1600
+        taxCalculatorResponse.outgoingVat == 0
+        taxCalculatorResponse.vatToPay == 1600
+
+    }
+
+    def "return correct value for buyer"() {
+        given:
+        addUniqueInvoices(5)
+
+        when:
+        def taxCalculatorResponse = calculateTax("14")
+
+        then:
+        taxCalculatorResponse.income == 0
+        taxCalculatorResponse.costs == 10000
+        taxCalculatorResponse.earnings == -10000
+        taxCalculatorResponse.incomingVat == 0
+        taxCalculatorResponse.outgoingVat == 800
+        taxCalculatorResponse.vatToPay == -800
+
+        when:
+        addUniqueInvoices(5)
+        taxCalculatorResponse = calculateTax("14")
+
+        then:
+        taxCalculatorResponse.income == 0
+        taxCalculatorResponse.costs == 20000
+        taxCalculatorResponse.earnings == -20000
+        taxCalculatorResponse.incomingVat == 0
+        taxCalculatorResponse.outgoingVat == 1600
+        taxCalculatorResponse.vatToPay == -1600
     }
 
     def "returned 0 if no invoices was added"() {
 
         when:
         def taxCalculatorResponse = calculateTax("0")
+
+        then:
+        taxCalculatorResponse.income == 0
+        taxCalculatorResponse.costs == 0
+        taxCalculatorResponse.earnings == 0
+        taxCalculatorResponse.incomingVat == 0
+        taxCalculatorResponse.outgoingVat == 0
+        taxCalculatorResponse.vatToPay == 0
+    }
+
+    def "returned 0 if invoice with given tax identification number was not found"() {
+        given:
+        addUniqueInvoices(5)
+
+        when:
+        def taxCalculatorResponse = calculateTax("20")
 
         then:
         taxCalculatorResponse.income == 0
@@ -73,11 +128,6 @@ class TaxCalculatorControllerIntegrationTest extends Specification {
                         .andReturn()
                         .response
                         .contentAsString)
-    }
-
-    private String convertToJson(int id) {
-        jsonService.toJson(TestHelpers.invoice(id))
-
     }
 
     private List<Invoice> addUniqueInvoices(int count) {
