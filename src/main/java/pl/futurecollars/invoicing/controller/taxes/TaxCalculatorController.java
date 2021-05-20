c5c47699615e2480b2d3cd044810eaf5a5735537
Paweł Kazanecki <kazan87@gmail.com>
2021-05-20 17:07:54 +0200

    Extracted annotations from TaxCalculatorController and replacing Builder in TaxCalculatorResponse with lombok annotation

--- src/main/java/pl/futurecollars/invoicing/controller/taxes/TaxCalculatorController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/taxes/TaxCalculatorController.java
@@ -1,23 +1,20 @@
 package pl.futurecollars.invoicing.controller.taxes;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.service.TaxCalculatorService;
 
-@RequestMapping("/tax")
 @RestController
-@Api(tags = {"Tax-Controller"})
 @AllArgsConstructor
-public class TaxCalculatorController {
+public class TaxCalculatorController implements TaxCalculatorApi {
 
     private TaxCalculatorService taxCalculatorService;
 
+    @Override
     @GetMapping(value = "/{taxIdentificationNumber}", produces = {"application/json;charset=UTF-8"})
     @ApiOperation(value = "Calculate incomes, costs and vat")
     public TaxCalculatorResponse calculateTaxes(@ApiParam(example = "555-555-55-55") @PathVariable String taxIdentificationNumber) {
