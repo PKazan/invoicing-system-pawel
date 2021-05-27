1c0c1e8d823d3a551d9869ac81a7110370d6a886
Paweł Kazanecki <kazan87@gmail.com>
2021-05-27 12:02:18 +0200

    Added empty row after class definition

--- src/main/java/pl/futurecollars/invoicing/model/Car.java
+++ src/main/java/pl/futurecollars/invoicing/model/Car.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class Car {
+
     @ApiModelProperty(value = "Car registration number", required = true, example = "WGM-5MN8")
     private String registration;
     @ApiModelProperty(value = "Information whether car is also used for privat purposes", required = true, example = "true")
