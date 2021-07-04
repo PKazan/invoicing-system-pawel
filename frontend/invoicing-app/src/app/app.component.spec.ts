09d33d8f10885bd49f0def23461a0dde842e4942
Paweł Kazanecki <kazan87@gmail.com>
2021-07-04 03:51:55 +0200

    Corrected tests

--- frontend/invoicing-app/src/app/app.component.spec.ts
+++ frontend/invoicing-app/src/app/app.component.spec.ts
@@ -1,5 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { Company } from './Company';
+import { CompanyService } from './company.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -7,7 +11,23 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      providers: [
+        { provide: CompanyService, useClass: MockCompanyService } 
+      ], 
+      imports: [
+        FormsModule
+      ]
     }).compileComponents();
+  });
+
+  it('should display a list of companies', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.ngOnInit()
+    fixture.detectChanges();
+    
+    expect(fixture.nativeElement.innerText).toContain("111-111-11-11 ul. First 1 First Ltd. 1111.11 111.11")
+    expect(fixture.nativeElement.innerText).toContain("222-222-22-22 ul. Second 2 Second Ltd. 2222.22 2222.22")
   });
 
   it('should create the app', () => {
@@ -16,16 +36,42 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'invoicing-app'`, () => {
+  it(`should have as title 'Invoicing App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('invoicing-app');
+    expect(app.title).toEqual('Invoicing App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('invoicing-app app is running!');
+    expect(compiled.querySelector('div.content').textContent).toContain('Hello Invoicing App!');
   });
 });
+
+class MockCompanyService {
+  
+  companies: Company [] = [
+    new Company(
+      1,
+      "111-111-11-11",
+      "ul. First 1",
+      "First Ltd.",
+      1111.11,
+      111.11
+    ),
+    new Company(
+      2,
+      "222-222-22-22",
+      "ul. Second 2",
+      "Second Ltd.",
+      2222.22,
+      222.22
+    )
+  ];  
+
+  getCompanies() {
+    return of(this.companies)
+  }
+}
